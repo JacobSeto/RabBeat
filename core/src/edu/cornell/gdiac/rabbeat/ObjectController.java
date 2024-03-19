35aa2af187e765170c765843abf63e893ef8e12f
yangerdanger27 <93808580+yangerdanger@users.noreply.github.com>
2024-03-18 20:25:50 -0400

    added in speed parameter to weighted platforms

--- core/src/edu/cornell/gdiac/rabbeat/ObjectController.java
+++ core/src/edu/cornell/gdiac/rabbeat/ObjectController.java
@@ -264,7 +264,7 @@ public class ObjectController {
             JsonValue currentWP = wplatjv.get(ii);
             WeightedPlatform obj;
             obj = new WeightedPlatform(currentWP.get("pos").asFloatArray(), currentWP.get("synthPos").asFloatArray(),
-                    currentWP.get("jazzPos").asFloatArray(), 1.0f);
+                    currentWP.get("jazzPos").asFloatArray(),  currentWP.getFloat("speed"));
             obj.setBodyType(BodyDef.BodyType.StaticBody);
             obj.setDensity(defaults.getFloat("density", 0.0f));
             obj.setFriction(defaults.getFloat("friction", 0.0f));
