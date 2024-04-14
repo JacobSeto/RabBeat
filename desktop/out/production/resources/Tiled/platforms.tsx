9611c6638a62022a53e7973ae8aa3c3dd5394160
JacobSeto <jacobtuxseto1234@gmail.com>
2024-04-14 13:49:30 -0400

    Alpha Build
    - renamed several refrences in assets.json due to .PNG ending being significant in reading the image files
    - rebuilt jar

---
+++ desktop/out/production/resources/Tiled/platforms.tsx
@@ -0,0 +1,40 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="platforms" tilewidth="192" tileheight="64" tilecount="12" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="9">
+  <image width="128" height="64" source="../world/platforms/jazzLongPlatform/jazzleft.PNG"/>
+ </tile>
+ <tile id="10">
+  <image width="128" height="64" source="../world/platforms/jazzLongPlatform/jazzmid.PNG"/>
+ </tile>
+ <tile id="11">
+  <image width="128" height="64" source="../world/platforms/jazzLongPlatform/jazzright.PNG"/>
+ </tile>
+ <tile id="12">
+  <image width="192" height="64" source="../world/platforms/jazzPlat.PNG"/>
+ </tile>
+ <tile id="13">
+  <image width="192" height="64" source="../world/platforms/platform.PNG"/>
+ </tile>
+ <tile id="14">
+  <image width="128" height="64" source="../world/platforms/synthLongPlatform/synthleft.PNG"/>
+ </tile>
+ <tile id="15">
+  <image width="128" height="64" source="../world/platforms/synthLongPlatform/synthmid.PNG"/>
+ </tile>
+ <tile id="16">
+  <image width="128" height="64" source="../world/platforms/synthLongPlatform/synthright.PNG"/>
+ </tile>
+ <tile id="17">
+  <image width="192" height="64" source="../world/platforms/synthPlat.PNG"/>
+ </tile>
+ <tile id="20">
+  <image width="128" height="64" source="../world/platforms/longPlatform/left.PNG"/>
+ </tile>
+ <tile id="21">
+  <image width="128" height="64" source="../world/platforms/longPlatform/mid.PNG"/>
+ </tile>
+ <tile id="22">
+  <image width="128" height="64" source="../world/platforms/longPlatform/right.PNG"/>
+ </tile>
+</tileset>
