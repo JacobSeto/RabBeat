fba4a7d84304310beaef6656bc9d12203f14b830
yangerdanger27 <93808580+yangerdanger@users.noreply.github.com>
2024-04-18 12:45:18 -0400

    Added rabeat level and updated moving/weighted platform functionaility.

--- core/src/edu/cornell/gdiac/rabbeat/obstacles/platforms/WeightedPlatform.java
+++ core/src/edu/cornell/gdiac/rabbeat/obstacles/platforms/WeightedPlatform.java
@@ -8,6 +8,7 @@ import edu.cornell.gdiac.rabbeat.Genre;
 import edu.cornell.gdiac.rabbeat.obstacles.BoxGameObject;
 import edu.cornell.gdiac.rabbeat.obstacles.IGenreObject;
 import edu.cornell.gdiac.rabbeat.obstacles.PolygonGameObject;
+import edu.cornell.gdiac.rabbeat.sync.ISynced;
 
 /**
  * WeightedPlatform.java
@@ -15,7 +16,7 @@ import edu.cornell.gdiac.rabbeat.obstacles.PolygonGameObject;
  * This class provides a weighted platform which changes location depending on the genre.
  */
 
-public class WeightedPlatform extends BoxGameObject implements IGenreObject {
+public class WeightedPlatform extends BoxGameObject implements IGenreObject, ISynced {
     /** Position for the weighted platform when the game is in Synth mode **/
     private Vector2 synthPosition;
 
@@ -34,6 +35,18 @@ public class WeightedPlatform extends BoxGameObject implements IGenreObject {
 
     /**The distance the platform should 'lock on' to its destination */
     private float LOCKDIST = 0.1f;
+
+    private float currentSpeed = 0;
+
+    private int beat = 0;
+
+    private int BPM = 180;
+
+    /**Determines the speed of the platform on each frame */
+    private float SPEEDBEAT6 = (float) 3*1 /10;
+    private float SPEEDBEAT7 = (float) 2*3 /10;
+    private float SPEEDBEAT8 = (float) 2*6 /10;
+
     /**
      * Creates a new weighted platform with the given physics data and current genre.
      *
@@ -102,7 +115,51 @@ public class WeightedPlatform extends BoxGameObject implements IGenreObject {
             setPosition(getPosition().x + velocity.x*delta*direction, getPosition().y + velocity.y*delta*direction);
         }
     }
+    /**Determines the distance between two vectors */
+    public float magnitude(Vector2 pos1, Vector2 pos2){
+        double magnitude = Math.sqrt(Math.pow((pos1.x - pos2.x),2)+
+                Math.pow((pos1.y-pos2.y),2));
+        return (float) magnitude;
+    }
+    /**Determines the normalized direction vector bewteen two vectors, with a a float multiplier*/
+    public Vector2 direction(Vector2 pos1, Vector2 pos2, float speed){
+        float magnitude = magnitude(pos1, pos2);
+        return new Vector2((pos1.x - pos2.x)*speed/magnitude,
+                (pos1.y-pos2.y)*speed/magnitude);
+    }
+    /**Calculates the time it takes to reach the next platform given the current speed*/
+//    public float solveDelta(float velocity){
+//        return magnitude(getPosition(), positionNodes[destination])/velocity;
+//    }
+    @Override
+    public float getBeat() {
+        /** 4 pulses every quarter note*/
+        return 4;
+    }
 
+    @Override
+    public void beatAction() {
+        /**Renable moving after reaching destination and incredments beat, as well as resetting the speed*/
+        moving = true;
+        float BeatLength = (float) 60 /BPM;
+        beat+= 1;
+        if (beat==1){
+            currentSpeed = 0;
+        }
+        else if (beat==6){
+            if (moving){
+                currentSpeed =1 * SPEEDBEAT6;
+            }
+            //currentSpeed = (magnitude(positionNodes[home], positionNodes[destination])*(1/BeatLength)*SPEEDBEAT6);
+        }
+        else if (beat== 7){
+            //currentSpeed = (magnitude(positionNodes[home], positionNodes[destination])*(1/BeatLength)*SPEEDBEAT7);
+        }
+        else if (beat == 8 ){
+            //currentSpeed = (magnitude(positionNodes[home], positionNodes[destination])*(1/BeatLength)*SPEEDBEAT8);
+            beat = 0;
+        }
+    }
     @Override
     public void genreUpdate(Genre genre) {
         move(genre);
@@ -127,17 +184,5 @@ public class WeightedPlatform extends BoxGameObject implements IGenreObject {
                 currentGenre = 0;
                 break;
         }
-    }
-    /**Determines the distance between two vectors */
-    public float magnitude(Vector2 pos1, Vector2 pos2){
-        double magnitude = Math.sqrt(Math.pow((pos1.x - pos2.x),2)+
-                Math.pow((pos1.y-pos2.y),2));
-        return (float) magnitude;
-    }
-    /**Determines the normalized direction vector bewteen two vectors, with a a float multiplier*/
-    public Vector2 direction(Vector2 pos1, Vector2 pos2, float speed){
-        float magnitude = magnitude(pos1, pos2);
-        return new Vector2((pos1.x - pos2.x)*speed/magnitude,
-                (pos1.y-pos2.y)*speed/magnitude);
     }
 }
