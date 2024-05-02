9283eb82d3902a26d15fb757f0d922d2cd4c71d4
yangerdanger27 <93808580+yangerdanger@users.noreply.github.com>
2024-05-02 13:16:16 -0400

    Made levels 4 and 5 easier, as well as compiled a jar file

---
+++ desktop/out/production/resources/Tiled/rabbeatTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="rabbeatTileset" tilewidth="64" tileheight="64" tilecount="21" columns="3">
+ <image source="tilesets/rabbeatTileset.png" width="192" height="448"/>
+</tileset>
