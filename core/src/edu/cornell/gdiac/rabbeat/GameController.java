c94bd8996bd3ea469e5dd733c584b9f6ba5aecc1
yangerdanger27 <93808580+yangerdanger@users.noreply.github.com>
2024-05-07 02:48:15 -0400

    levels

--- core/src/edu/cornell/gdiac/rabbeat/GameController.java
+++ core/src/edu/cornell/gdiac/rabbeat/GameController.java
@@ -1179,7 +1179,8 @@ public class GameController implements Screen, ContactListener {
 
 	/** Sets the integer levelsUnlocked */
 	public void setLevelsUnlocked(int levelsUnlocked) {
-		this.levelsUnlocked = levelsUnlocked;
+		// TODO: CHANGE THIS BACK TO levelsUnlocked
+		this.levelsUnlocked = 12;
 	}
 
 	/** Increments the integer levelsUnlocked if a player completes a level and the next level is locked*/
