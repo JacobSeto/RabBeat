3961c1910e5b453b2b41a56e73744d9a1a346495
Caitlyn Jin <cj384@cornell.edu>
2024-05-18 06:07:02 -0400

    Added checkpoint rising animation

--- core/src/edu/cornell/gdiac/rabbeat/ObjectController.java
+++ core/src/edu/cornell/gdiac/rabbeat/ObjectController.java
@@ -2,6 +2,7 @@ package edu.cornell.gdiac.rabbeat;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
