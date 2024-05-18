bfd24433910d66f5961e3c55c04a9cc36fc54a64
Emily <emily.silkin@gmail.com>
2024-05-18 09:26:13 -0400

    Imported level name ui

--- core/src/edu/cornell/gdiac/rabbeat/GameController.java
+++ core/src/edu/cornell/gdiac/rabbeat/GameController.java
@@ -1265,6 +1265,66 @@ public class GameController implements Screen, ContactListener {
 
 		canvas.end();
 
+		canvas.begin(true);
+
+		if(genre == Genre.SYNTH) {
+			switch(currentLevelInt) {
+				case(1):
+					break;
+				case(2):
+					break;
+				case(3):
+					break;
+				case(4):
+					break;
+				case(5):
+					break;
+				case(6):
+					break;
+				case(7):
+					break;
+				case(8):
+					break;
+				case(9):
+					break;
+				case(10):
+					break;
+				case(11):
+					break;
+				case(12):
+					break;
+			}
+		} else if(genre == Genre.JAZZ) {
+			switch(currentLevelInt) {
+				case(1):
+					break;
+				case(2):
+					break;
+				case(3):
+					break;
+				case(4):
+					break;
+				case(5):
+					break;
+				case(6):
+					break;
+				case(7):
+					break;
+				case(8):
+					break;
+				case(9):
+					break;
+				case(10):
+					break;
+				case(11):
+					break;
+				case(12):
+					break;
+			}
+		}
+
+		canvas.end();
+
 		// Put pause screen UI in this if statement
 		if (paused) {
 			float pulse = syncController.uiSyncPulse.uiPulseScale;
